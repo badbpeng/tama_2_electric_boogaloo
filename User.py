d501f689f8912e03315118c3de92d3a3b4e9c413
Adam Beck <beck065@csusm.edu>
2023-02-13 22:11:22 -0800

    update User.py

--- User.py
+++ User.py
@@ -1,16 +1,34 @@
 import pickle
+import time
 import Pet
 
 class User:
-    # TODO: properly dump the pet 
-    def __init__(self):
-        self.pet = self.give_pet()
-        pickle.dump(self.pet)
+    def __init__(self, phone_number):
+        # TODO after bug fixing make vars private
+        self.pet = self.give_pet() # make the pet
+        self.time = time.time() # record the time of pet creation [Every time the pet is updated, record the time]
+        self.phone = phone_number # save the user's phone number
 
-    # TODO: add rng, grab name and noise from database
+        # dump the inital user class as a pickle in a file named after their phone 
+        with open(phone_number, 'wb') as p:
+            pickle.dump(self, p)
+
+    # TODO: add rng, grab pet's name and noise from database
     def give_pet(self):
         return Pet("test_name", "test_sound")
 
-    # TODO: check the latest time the pet has been unpickled
-    def check_time(self):
-        return
+    # game logic will need to load this user's pickle before calling this function, key for every user's pickle is thier phone #
+    # checks if the pet's values needs to be updated
+    def check_pet(self):
+        # checks if it's been over an hour since the pet has been updated
+        if (time.time() - self.time > 3600):
+            # decrease all of the pets values
+            self.pet.decrease_hunger()
+            self.pet.decrease_health()
+            self.pet.decrease_happiness()
+            # record the time
+            self.time = time.time()
+
+            # update the pet
+            with open(self.phone, 'wb') as p:
+                pickle.dump(self, p)
